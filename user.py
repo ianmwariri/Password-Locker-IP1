1beb4d4d97eaf28c207fb16f74a13e7bb4450e14
Ian Mwariri <mwangi.ian3@gmail.com>
2020-09-10 12:09:54 +0300

    Add Several methods to Credentials Class.

--- user.py
+++ user.py
@@ -1,14 +1,14 @@
 class Credentials:
     #Defining a list that will contain all user credentials
-    cred_list=[]
+    credList=[]
    
     def save_credentials(self):
-        Credentials.cred_list.append(self)
+        Credentials.credList.append(self)
     
     def rub_credentials(self):
-        Credentials.cred_list.remove(self)
+        Credentials.credList.remove(self)
 
-    def __init__ (self, username,accountName,accountPassword)
+    def __init__ (self, username,accountName,accountPassword):
         self.id=id
         self.username=username
         self.accountName=accountName
@@ -16,8 +16,19 @@ class Credentials:
 
     @classmethod
     def locateAccount(allCreds, acc_name):
-        for cred in allCreds.cred_list:
+        for cred in allCreds.credList:
             if cred.accountName==acc_name:
                 return cred.accountPassword
     
+    @classmethod 
+    def show_cred(allCreds):
+        return allCreds.credList
+    
+    @classmethod
+    def available_creds(allCreds,acc_name):
+        for cred in allCreds.credList:
+            if cred.accountName==acc_name:
+                 return True
+    
+        return False
    
